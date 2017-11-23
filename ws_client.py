b814d1d7e314abd330e5f1a4e447eb49acb6705c
tmust <rslu3000@gmail.com>
2017-11-23 21:18:09 +0800

    first commit

---
+++ ws_client.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#python3
+import asyncio
+import websockets
+
+async def hello():
+    async with websockets.connect('ws://localhost:5678') as websocket:
+        name = input("What's your name? ")
+        await websocket.send(name)
+        print("> {}".format(name))
+
+        greeting = await websocket.recv()
+        print("< {}".format(greeting))
+
+while True:
+    asyncio.get_event_loop().run_until_complete(hello())
